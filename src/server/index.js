af054aef890411d6ae9d4eed6a33ffb41c472349
Dieter Jackson <jacksondieter@gmail.com>
2021-08-08 02:07:57 +0200

    server: Add api calls

--- src/server/index.js
+++ src/server/index.js
@@ -11,19 +11,46 @@ app.use(express.json())
 
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-// your API calls
+// API calls
 
-// example API call
-app.get('/api/apod', async (req, res) => {
+app.get('/api/rovers', async (req, res) => {
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        const data = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
-        res.send({ image })
+            .then(roversFilter)
+        res.send({ data })
     } catch (err) {
         console.log('error:', err);
     }
 })
 
+app.get('/api/rovers/:rover', async (req, res) => {
+    const {rover} = req.params
+    try {
+        const data = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+            .then(photoFilter)
+        res.send({ data })
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+const roversFilter = (data) => {
+    const rovers = data.rovers.map(rover => {
+        const {name, landing_date, launch_date, status,cameras, max_date, max_sol, total_photos} = rover
+        const total_cam = cameras.length
+        return {name,landing_date,launch_date,status, max_date, max_sol, total_photos, total_cam}
+    })
+    return rovers
+}
+
+const photoFilter = (data) => {
+    const {latest_photos:photos} = data
+    return photos.filter((_,ind)=>(ind < 12)).map(photo => {
+        const {camera:{full_name:cam_name},img_src:img, earth_date:date} = photo
+        return {cam_name, date, img}
+    })
+}
